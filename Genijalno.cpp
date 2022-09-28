d2748bbcfc1db8132f92847ffb601dac83ef3f33
Adriano <95847404+NisamInvalid@users.noreply.github.com>
2022-09-28 12:00:41 +0200

    Add files via upload

---
+++ Genijalno.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main(){
+	int a=0, i=0;
+	
+	while (a==0){
+		int j=0;
+		i++;
+	for(j=0;j<i;j++) printf(" ");
+	printf ("AdrianoJeCool\n"); //by Adriano
+	}
+}
+	
